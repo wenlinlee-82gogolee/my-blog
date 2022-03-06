552fe5cdf443e756dda095a954e5a2c961ce6815
Wen-Lin Lee <82gogolee@gmail.com>
2022-03-06 15:25:45 +0800

    add DetailView create the getPost composable

---
+++ src/composables/getPost.js
@@ -0,0 +1,23 @@
+import { ref } from 'vue';
+
+const getPost = id => {
+  const post = ref(null);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let data = await fetch('http://localhost:3000/posts/' + id);
+      post.value = await data.json();
+      if (!data.ok) {
+        throw Error('that post does not exist...');
+      }
+    } catch (err) {
+      error.value = err.message;
+      console.log(err.message);
+    }
+  };
+
+  return { post, error, load };
+};
+
+export default getPost;
